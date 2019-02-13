ed9addb919f648be22a68359ffd7e58ac237b3ac
srikutty19 <sri9132@gmail.com>
2019-02-13 19:28:18 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 var krms_config ={			
-	'ApiUrl':"",	
+	'ApiUrl':"http://foodieschoice.tk/mobileapp/api",	
 	'DialogDefaultTitle':"KMRS",	
 	'pushNotificationSenderid':"",	
 	'facebookAppId':"", 
-	'APIHasKey':""
+	'APIHasKey':"2b0926cd0759663bca9051fca11a619e"
 };
